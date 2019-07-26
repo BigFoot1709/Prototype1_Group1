0c75d53fa689b19ab1cf5a0edd68ff3ebddfd03f
UltimateN00b <leia@gillyweed.co.za>
2019-07-26 19:47:40 +0200

    Added in subtitle system.

--- Assets/FocusCamera.cs
+++ Assets/FocusCamera.cs
@@ -5,36 +5,50 @@ using UnityEngine;
 public class FocusCamera : MonoBehaviour
 {
 
-    public float originalFieldOfView;
+    private float _originalFieldOfView;
     public float speed;
+    public float rotateSpeed;
     public float offset;
+    public float maxZoom;
 
     private Transform _zoomObject;
     private bool _startZoom;
 
     private GameObject _freeLookCamera;
+    private Transform _originalTransform;
 
     // Start is called before the first frame update
     void Start()
     {
-        originalFieldOfView = this.GetComponent<Camera>().fieldOfView;
+        _originalFieldOfView = this.GetComponent<Camera>().fieldOfView;
+        _originalTransform = this.GetComponent<Transform>();
         _startZoom = false;
         _freeLookCamera = GameObject.Find("FreeLookCameraRig");
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         if (_startZoom)
         {
-            //this.transform.position = Vector3.MoveTowards(this.transform.position, _zoomObject.position, speed * Time.deltaTime);
             Vector3 targetPos = _zoomObject.position;
             targetPos.z -= offset;
             targetPos.y -= offset;
 
-            this.transform.position = targetPos;
+            this.transform.position = Vector3.MoveTowards(this.transform.position, targetPos, speed * Time.deltaTime);
 
-            if (this.GetComponent<Camera>().fieldOfView > 30)
+            Vector3 targetDir = _zoomObject.position - transform.position;
+
+            // The step size is equal to speed times frame time.
+            float step = rotateSpeed * Time.deltaTime;
+
+            Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0.0f);
+            //Debug.DrawRay(transform.position, newDir, Color.red);
+
+            // Move our position a step closer to the target.
+            transform.rotation = Quaternion.Lerp(this.transform.rotation, Quaternion.LookRotation(newDir), Time.deltaTime * rotateSpeed);
+
+            if (this.GetComponent<Camera>().fieldOfView > maxZoom)
             {
                 this.GetComponent<Camera>().fieldOfView -= 0.1f;
             } else
@@ -51,7 +65,16 @@ public class FocusCamera : MonoBehaviour
 
     public void FocusOnObject(Transform newZoomObject)
     {
-        //this.transform.position = GameObject.Find("FreeLookCameraRig").transform.position;
+
+       GameObject mainCamera =  _freeLookCamera.transform.GetChild(0).transform.GetChild(0).gameObject;
+        mainCamera.transform.parent = null;
+
+        this.transform.localPosition = _freeLookCamera.transform.localPosition;
+        this.transform.rotation = mainCamera.transform.rotation;
+        this.transform.localScale = _freeLookCamera.transform.localScale;
+
+        mainCamera.transform.SetParent(_freeLookCamera.transform.GetChild(0));
+        
         _zoomObject = newZoomObject;
         _startZoom = true;
     }
@@ -61,7 +84,7 @@ public class FocusCamera : MonoBehaviour
         if (this.GetComponent<Camera>().enabled)
         {
             _freeLookCamera.SetActive(true);
-            this.GetComponent<Camera>().fieldOfView = originalFieldOfView;
+            this.GetComponent<Camera>().fieldOfView = _originalFieldOfView;
             this.GetComponent<Camera>().enabled = false;
         }
     }
---
+++ Assets/Subtitles.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Subtitles : MonoBehaviour
+{
+    private List<string> _currLines;
+    private int _index;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<Text>().text = "";
+        _index = 0;
+        //_currConversation = GameObject.Find("ConversationHolder").GetComponent<Conversation>().GetLines();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            PlayConversation("Harry Potter");
+        }
+    }
+
+    public void NextLine()
+    {
+        this.GetComponent<Text>().text = _currLines[_index];
+        if (_index < _currLines.Count)
+        {
+            _index++;
+        }
+    }
+
+    public void SetCurrConversation(List<string> newConvo)
+    {
+        _currLines = newConvo;
+    }
+
+    public void Empty()
+    {
+
+    }
+
+    public void PlayConversation(string name)
+    {
+
+        Conversation currentConversation = GetConversationByName(name);
+        _currLines = currentConversation.GetLines();
+
+        GameObject.Find("DialogueSoundSource").GetComponent<DialogueSoundSource>().PlayDialogue(currentConversation.GetClip(), currentConversation.GetAnimator());
+    }
+
+    public Conversation GetConversationByName(string name)
+    {
+        Conversation[] conversations = GameObject.Find("ConversationHolder").GetComponents<Conversation>();
+
+        Conversation returnConvo = null;
+
+        foreach (Conversation c in conversations)
+        {
+            if (c.GetName().ToUpper().Contains(name.ToUpper()))
+            {
+                returnConvo = c;
+            }
+        }
+
+        return returnConvo;
+    }
+}
