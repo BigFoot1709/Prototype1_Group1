d4013262b518763218f9709048482b6e8982a9df
UltimateN00b <leia@gillyweed.co.za>
2019-07-27 21:59:46 +0200

    Made some trees move.

---
+++ Assets/Road.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Road : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 targetPos = this.transform.position;
+        targetPos.z = GameObject.Find("SwitchMarker").transform.position.z;
+        this.transform.position = Vector3.Lerp(this.transform.position, targetPos, 0.1f * Time.deltaTime);
+    }
+}
---
+++ Assets/TreeMaker.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeMaker : MonoBehaviour
+{
+
+    public GameObject tree;
+    public int gap;
+    public int treeNum;
+
+    private float _zPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _zPos = this.transform.position.z;
+
+        for (int i = 0; i < treeNum; i++)
+        {
+            Vector3 pos = this.transform.position;
+            _zPos += gap;
+            pos.z = _zPos;
+            Instantiate(tree, pos, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
