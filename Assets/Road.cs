58dd0094d1eefc77e5280242c180c74c1b7f3f58
UltimateN00b <leia@gillyweed.co.za>
2019-07-28 17:11:18 +0200

    Made road scroll repeatedly.

--- Assets/Road.cs
+++ Assets/Road.cs
@@ -13,8 +13,22 @@ public class Road : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 targetPos = this.transform.position;
-        targetPos.z = GameObject.Find("SwitchMarker").transform.position.z;
-        this.transform.position = Vector3.Lerp(this.transform.position, targetPos, 0.1f * Time.deltaTime);
+        transform.position += Vector3.forward * 100* Time.deltaTime;
+
+        if (this.transform.position.z >= GameObject.Find("BeginningMarker").transform.position.z)
+        {
+            Vector3 myPos = this.transform.position;
+
+            if (this.gameObject.name.Equals("Road2"))
+            {
+                GameObject endMarker = Utilities.SearchChild("EndMarker", GameObject.Find("Road1"));
+                myPos.z = endMarker.transform.position.z;
+            } else
+            {
+                GameObject endMarker = Utilities.SearchChild("EndMarker", GameObject.Find("Road2"));
+                myPos.z = endMarker.transform.position.z;
+            }
+            this.transform.position = myPos;
+        }
     }
 }
