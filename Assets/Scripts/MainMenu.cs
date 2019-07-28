9cdf64eada50cec025bf42958f7f409d97955d0d
Keaton de Jager <keatondejager@gmail.com>
2019-07-28 21:25:11 +0200

    Main Menu, and everything else

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{   
+
+    public GameObject startDisp;
+    public GameObject quitDisp;
+    public void StartGame () {
+        startDisp.SetActive(true);
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame () {
+        quitDisp.SetActive(true);
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/QuttingControl.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuttingControl : MonoBehaviour
+{
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+        }
+    }
+}
